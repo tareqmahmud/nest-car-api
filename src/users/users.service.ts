9619ad0b4b1b4e72f8304cb03e08a32afd8a4ae1
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2021-08-05 02:46:17 +0600

    Add user signup with basic save

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string) {
+    const user = await this.userRepository.create({ email, password });
+
+    return this.userRepository.save(user);
+  }
+}
